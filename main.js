9236fc066c760579d28bda55653b6cab1be4de3d
Rohith19 <rohith1512a@gmail.com>
2023-04-18 14:43:30 +0530

    Mouseclick and hold to draw feature added

--- main.js
+++ main.js
@@ -2,40 +2,49 @@ let color = "black";
 let click = false;
 let sizeValue = document.getElementById("sizeValue");
 let sizeAdjust = document.querySelector("#sizeAdjust");
-let size = 0;
+let size = 16;
+let board = document.querySelector(".board");
 
-document.addEventListener("DOMContentLoaded", function() { //DOMContentLoaded : To load HTML and CSS Before executing JS
-    createBoard(16);//Default board size
+createBoard(size);
 
-    document.querySelector("body").addEventListener('click', function(e) {
-        if(e.target.tagName != "BUTTON" && e.target.tagName != "INPUT"){
-            click = !click;
-            let draw = document.querySelector("#draw");
-            if(click){
-                draw.innerText = "Now You can Draw.";
-            }else {
-                draw.innerText = "You're not allowed to draw.";
-            }
+board.addEventListener('mousedown', function(e) {
+    if(e.target.tagName != "BUTTON" && e.target.tagName != "INPUT"){
+        click = true;
+        let draw = document.querySelector("#draw");
+        if(click){
+            draw.innerText = "Now You can Draw.";
+        }else {
+            draw.innerText = "You're not allowed to draw.";
         }
-    })
-    sizeAdjust.addEventListener("mousemove", function(e) {
-        size = e.target.value;
-        sizeValue.innerHTML = `${size}x${size}`;
-        createBoard(size);
-    });
+    }
+});
+board.addEventListener('mouseup', function(e) {
+    if(e.target.tagName != "BUTTON" && e.target.tagName != "INPUT"){
+        click = false;
+        let draw = document.querySelector("#draw");
+        if(click){
+            draw.innerText = "Now You can Draw.";
+        }else {
+            draw.innerText = "You're not allowed to draw.";
+        }
+    }
+})
+sizeAdjust.addEventListener("click", function(e) {
+    resetBoard();
+    size = e.target.value;
+    sizeValue.innerText = `${size}x${size}`;
+    createBoard(size);
 });
 
 function createBoard(size) {
-    let board = document.querySelector(".board");
     //Set board into 16 * 16 grid
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
     let numDivs = size * size;//Size of the board
     //Loop through every div to create a div
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div");//Create a new div
-        div.addEventListener("mouseover", colorDiv);
+        div.addEventListener("mousemove", colorDiv);
         board.insertAdjacentElement("beforeend", div);
     }
 
@@ -57,6 +66,7 @@ function setColor(colorChoice) {
 }
 
 function resetBoard() {
-    let divs = document.querySelectorAll("div");
+    let divs = board.querySelectorAll("div");
     divs.forEach((div) => div.style.backgroundColor = "white");
 }
+
